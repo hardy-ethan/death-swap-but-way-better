09143cd1bb0222ed5094436af7a8faec5a7d38d0
Ethan Hardy <102838865+hardy-ethan@users.noreply.github.com>
2026-06-27 11:29:52 -0700

    !

--- fabric-mod/src/main/java/com/deathswap/game/GameManager.java
+++ fabric-mod/src/main/java/com/deathswap/game/GameManager.java
@@ -1081,7 +1081,7 @@ public final class GameManager {
         } else {
             broadcast(">> The game ended in a draw. <<", ChatFormatting.YELLOW);
         }
-        broadcast(">> Game lasted " + formatClock(gameTicksElapsed / 20) + ". <<", ChatFormatting.GRAY);
+        broadcast(">> Game lasted " + formatClock(gameTicksElapsed / 20) + "! <<", ChatFormatting.GRAY);
     }
 
     /** Abort the current game (operator /deathswap stop) and reset to the lobby. */
